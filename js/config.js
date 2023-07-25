203eae50ad0ea394668333ede8765b184fc1b296
Elusive-Ophidian <119871371+Elusive-Ophidian@users.noreply.github.com>
2023-07-25 00:50:55 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -9,7 +9,7 @@
 
 /*URL for the server logo
 Leave it blank to use text instead*/
-var l_serverImage = "https://ibb.co/511fCN6";
+var l_serverImage = "images/logo.jpg";
 
 /*Custom server name configuration
 Leave it blank for automatic server name*/
